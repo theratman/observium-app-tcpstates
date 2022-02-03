64669fccca06a1ac35e9319d4421ba0af9f15f67
Original RatMan <the.original.ratman@gmail.com>
2022-02-03 15:30:49 -0400

    Create apps.inc.php

---
+++ includes/definitions/apps.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+$config['app']['tcpstates']['states']            = array('CLOSE-WAIT', 'CLOSED', 'CLOSING', 'ESTAB', 'FIN-WAIT-1', 'FIN-WAIT-2', 'LAST-ACK', 'LISTEN', 'SYN-RECV', 'SYN-SENT', 'TIME-WAIT');
+
+// EOF
