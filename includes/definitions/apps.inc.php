996e9b6dc7df58fc8a832dffc0ef4879c6d8800e
Original RatMan <the.original.ratman@gmail.com>
2022-02-04 15:35:53 -0400

    Update apps.inc.php

--- includes/definitions/apps.inc.php
+++ includes/definitions/apps.inc.php
@@ -1,5 +1,5 @@
 <?php
 
-$config['app']['tcpstates']['states']            = array('CLOSE-WAIT', 'CLOSED', 'CLOSING', 'ESTAB', 'FIN-WAIT-1', 'FIN-WAIT-2', 'LAST-ACK', 'LISTEN', 'SYN-RECV', 'SYN-SENT', 'TIME-WAIT');
+$config['app']['tcpstates']['top']            = array('states');
 
 // EOF
