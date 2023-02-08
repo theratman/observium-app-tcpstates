2a881ee68ec7cd59161b8a114dce5f310db1a1ff
Original RatMan <the.original.ratman@gmail.com>
2023-02-08 09:09:19 -0400

    Update rrdtypes.inc.php
    Fixing for running on PHP 8.1

--- includes/definitions/rrdtypes.inc.php
+++ includes/definitions/rrdtypes.inc.php
@@ -3,17 +3,17 @@
 $config['rrd_types']['tcpstates'] = array(
   'file'  => 'app-tcpstates-%index%.rrd',
   'ds'    => array(
-    'CLOSE-WAIT'          => array('ds_type' => 'GAUGE',  'ds_min' => 0),
-    'CLOSED'          => array('ds_type' => 'GAUGE',  'ds_min' => 0),
-    'CLOSING'          => array('ds_type' => 'GAUGE',  'ds_min' => 0),
-    'ESTAB'          => array('ds_type' => 'GAUGE',  'ds_min' => 0),
-    'FIN-WAIT-1'          => array('ds_type' => 'GAUGE',  'ds_min' => 0),
-    'FIN-WAIT-2'          => array('ds_type' => 'GAUGE',  'ds_min' => 0),
-    'LAST-ACK'          => array('ds_type' => 'GAUGE',  'ds_min' => 0),
-    'LISTEN'          => array('ds_type' => 'GAUGE',  'ds_min' => 0),
-    'SYN-RECV'          => array('ds_type' => 'GAUGE',  'ds_min' => 0),
-    'SYN-SENT'          => array('ds_type' => 'GAUGE',  'ds_min' => 0),
-    'TIME-WAIT'          => array('ds_type' => 'GAUGE',  'ds_min' => 0),
+    'CLOSE-WAIT'          => array('type' => 'GAUGE',  'min' => '0', 'max' => '1048576'),
+    'CLOSED'              => array('type' => 'GAUGE',  'min' => '0', 'max' => '1048576'),
+    'CLOSING'             => array('type' => 'GAUGE',  'min' => '0', 'max' => '1048576'),
+    'ESTAB'               => array('type' => 'GAUGE',  'min' => '0', 'max' => '1048576'),
+    'FIN-WAIT-1'          => array('type' => 'GAUGE',  'min' => '0', 'max' => '1048576'),
+    'FIN-WAIT-2'          => array('type' => 'GAUGE',  'min' => '0', 'max' => '1048576'),
+    'LAST-ACK'            => array('type' => 'GAUGE',  'min' => '0', 'max' => '1048576'),
+    'LISTEN'              => array('type' => 'GAUGE',  'min' => '0', 'max' => '1048576'),
+    'SYN-RECV'            => array('type' => 'GAUGE',  'min' => '0', 'max' => '1048576'),
+    'SYN-SENT'            => array('type' => 'GAUGE',  'min' => '0', 'max' => '1048576'),
+    'TIME-WAIT'           => array('type' => 'GAUGE',  'min' => '0', 'max' => '1048576'),
   ),
 );
 
